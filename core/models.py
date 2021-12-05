d8446a32e73a55a2f18952f5499fd2073527aeee
Bogdan <soaqa57@gmail.com>
2021-12-05 16:42:03 +0300

    Добавил комментарии и переделал модели с тестом.

--- core/models.py
+++ core/models.py
@@ -4,12 +4,15 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
-# # Create your models here.
+# Создадим модель нашего покупателя, просто унаследовав пользователя Django
 class Buyer(AbstractUser):
     ...
 
 
 class PriceMixin(models.Model):
+    """
+        Абстрактная родительская таблица для общих полей и методов, чтобы не повторяться
+    """
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
@@ -17,40 +20,64 @@ class PriceMixin(models.Model):
 
 
 class Pan(PriceMixin):
-    vendor_choices = [("tefal", "Tefal")]
+    """
+        Модель описывающая таблицу с товаром типа: Сковорода
+    """
+    VENDOR_TEFAL = "tefal", "Tefal"
+
+    vendor_choices = [(VENDOR_TEFAL[0], VENDOR_TEFAL[1])]
     vendor = models.CharField(max_length=128, choices=vendor_choices)
     diameter = models.PositiveIntegerField()
-    purchases = GenericRelation("Purchase", related_name="gfc_purchases")
+
+    purchases = GenericRelation("Purchase", related_query_name="pan", related_name="pans")
 
     def __str__(self):
-        return f"Сковорода фирмы: {self.vendor}"
+        return f"Сковорода фирмы: {self.get_vendor_display()}, {self.diameter} sm"
 
 
 class Potato(PriceMixin):
-    country_choices = [("bel", "Буларусь")]
+    """
+        Модель описывающая таблицу с товаром типа: Картофель
+    """
+
+    COUNTRY_BEL = "bel", "Беларусь"
+    country_choices = [(COUNTRY_BEL[0], COUNTRY_BEL[1]), ]
+
     id = models.UUIDField(primary_key=True)
-    country = models.CharField(max_length=128)
-    purchases = GenericRelation("Purchase", related_name="gfc_purchases")
+    country = models.CharField(max_length=128, choices=country_choices)
+
+    purchases = GenericRelation("Purchase", related_query_name="potato", related_name="potatoes")
 
     def __str__(self):
-        return f"Картошка из страны: {self.country}"
+        return f"Картофель из страны: {self.get_country_display()}"
 
 
 class Purchase(models.Model):
+    """
+        Объект описывающий сущность покуаки чего либо.
+    """
     created_at = models.DateTimeField(auto_now_add=True)
     count = models.PositiveIntegerField()
-    content_type = models.ForeignKey(ContentType, on_delete=models.RESTRICT)
-    object_id = models.CharField(max_length=128)
     order = models.ForeignKey("Order", on_delete=models.CASCADE, related_name="purchases")
 
+    # Согласно документации, создаём поля для хранения ContentType и object_id
+    # https://docs.djangoproject.com/en/3.2/ref/contrib/contenttypes/#django.contrib.contenttypes.fields.GenericForeignKey
+    content_type = models.ForeignKey(ContentType, on_delete=models.RESTRICT)
+    object_id = models.CharField(max_length=128)
+
+    # Создаём генерируемый внешний ключ, который может быть связан с продуктами разных типов: сковорода, картошка и тд
     product = GenericForeignKey("content_type", "object_id")
 
     def __str__(self):
-        return f"purchase_id: {self.id}, CT: {self.content_type}, object_id: {self.object_id}"
+        # кстати тут у нас явный пример полиморфизма, у всех продуктов будет вызван метод __str__, но вернётся разный
+        # результат для каждого продукта
+        return f"purchase_id: {self.id}, product: {self.product}"
 
 
 class Order(models.Model):
+    """
+        Модель описывающая заказ, содержит связь с покупками, через related_name="purchases", см core/models.py:52
+    """
     buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
--- core/tests/test_purchases.py
+++ core/tests/test_purchases.py
@@ -10,17 +10,32 @@ class PurchasesTest(TestCase):
     POTATO_UUID = "b5a32d62-5535-11ec-a799-049226578ae5"
 
     def test_order_creation(self):
+        # Создаём покупателя, сковороду и белорусскую картошку :)
         buyer = Buyer.objects.create(id=1, username="Alex")
-        pan = Pan.objects.create(id=1, vendor='tefal', diameter=30, price=1500)
-        potato = Potato.objects.create(id=self.POTATO_UUID, country="bel", price=45.5)
+        pan = Pan.objects.create(id=1, vendor=Pan.VENDOR_TEFAL[0], diameter=30, price=1500)
+        potato = Potato.objects.create(id=self.POTATO_UUID, country=Potato.COUNTRY_BEL[0], price=45.5)
+
+        # Создаем заказ
         order = Order.objects.create(buyer=buyer)
 
+        # получим ContentType для сковороды и картошки
         ct_pan, ct_potato = ContentType.objects.get_for_model(Pan), ContentType.objects.get_for_model(Potato)
 
-        Purchase.objects.create(
-            count=3, content_type=ct_potato, object_id=potato.id, order=order
-        )
+        # Создадим две покупки, 3 кг картошки, 1 сковороды
+        Purchase.objects.create(count=3, content_type=ct_potato, object_id=potato.id, order=order)
         Purchase.objects.create(count=1, content_type=ct_pan, object_id=pan.id, order=order)
 
-        print(order.purchases.all())
+        # Првоерим что покупки привязались к заказу
         self.assertEqual(order.purchases.count(), 2)
+
+        # Теперь мы можем вывести все покупки товара картофель или сковрода
+        print(pan.purchases.all())
+        print(potato.purchases.all())
+
+        # Аналогично все покупки нашего заказа
+        print(order.purchases.all())
+
+        # Или даже все сковороды в заказе от заданного производителя
+        print(order.purchases.filter(pan__vendor="tefal"))
+        # <QuerySet [<Purchase: purchase_id: 1, CT: core | potato, object_id: b5a32d62-5535-11ec-a799-049226578ae5>,
+        #            <Purchase: purchase_id: 2, CT: core | pan, object_id: 1>]>
