c20ea827a3e52f36e7cf2330ee8cd59ddb63ff5a
Bogdan <soaqa57@gmail.com>
2022-01-05 17:59:56 +0300

    add square_of_a method

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def square_of_a(request):
+    a = request.GET.get("a")
+    if str(a).isdigit():
+        return HttpResponse(content=f"a in square is {int(a) ** 2}")
+    return HttpResponse(content=f"a is not a digit, or not presented")
