b3a44e50800fbd5957560a089daa6dc228af07b2
soaqa <soaqa57@gmail.com>
2023-03-24 18:56:37 +0300

    modify Buyer

--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKe
 from django.contrib.contenttypes.models import ContentType
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 # Создадим модель нашего покупателя, просто унаследовав пользователя Django
@@ -12,7 +13,33 @@ from django.urls import reverse
 
 
 class Buyer(AbstractUser):
-    ...
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[AbstractUser.username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+        null=True,
+        blank=True,
+    )
+
+    email = models.EmailField(_("email address"), unique=True, )
+
+    is_active = models.BooleanField(
+        _("active"),
+        default=False,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
 
 class PriceMixin(models.Model):
